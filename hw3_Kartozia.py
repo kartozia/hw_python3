894981e0d26cb8e8461293e4b31a81cfb2571f1b
kartozia <inga.kartoziya@gmail.com>
2017-02-15 19:01:43 +0100

    in progress

---
+++ hw3_Kartozia.py
@@ -0,0 +1,33 @@
+class Stack:
+     def __init__(self):
+          self.items = []
+
+     def isEmpty(self):
+          if self.items == []:
+               return True
+          else:
+               return False
+
+     def push(self, item):
+          self.items.append(item)
+
+     def pop(self):  # удалить элемент из стека и вернуть его значение
+          element = self.items.pop()
+          return self.items[element]
+
+     def peek(self):
+         element = self.items[len(self.items)-1]
+         return self.items[element]
+
+class MyQueue(Stack):
+    def __init__(self):
+         self.que = []
+         first = Stack()
+         second = Stack()
+
+     def make_queue(self, item):
+          
+
+ берем массив из одного стека и по очереди добавляем элементы в другой
+
+    
